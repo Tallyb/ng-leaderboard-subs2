1ec5e88b3e88e6a8e54f8df3bd8eb3586f15a447
Tally Barak <tally.barak@gmail.com>
2015-06-03 23:02:48 +0300

    stop subscription button

--- client/leaderboard.ng.js
+++ client/leaderboard.ng.js
@@ -3,25 +3,29 @@
  */
 
 angular.module('LeaderBoard',[
-    'angular-meteor',
-    'ngMaterial',
-    'ui.router',
-    'ngAnimate'
+    'angular-meteor'
 ])
 
 .controller ('leaderBoardCtrl', function ($scope, $meteor){
 
-    $scope.$meteorSubscribe('topPlayers').then(function(handle) {
+    var handles = [];
+    $meteor.subscribe('topPlayers').then(function(handle) {
+        handles [0] = handle;
         $scope.topPlayers = $meteor.collection(function (){
           return Players.find ({}, {sort: {score: -1}, limit: 2});
         });
     });
 
-    $scope.$meteorSubscribe('firstPlayers').then(function(handle) {
+    $meteor.subscribe('firstPlayers').then(function(handle) {
+        handles [1] = handle;
         $scope.firstPlayers = $meteor.collection(function (){
             return Players.find ({}, {sort: {name: 1}, limit: 3});
         });
     });
+
+    $scope.stopHandle = function (handleId){
+        handles [handleId].stop();
+    }
 
 });
 
